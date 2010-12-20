122bebd5474c413bf2d1ef60788f2e5f639270da
dliu <david.c.liu@facebook.com>
2010-12-20 15:04:13 -0800

    Synchronization on ResourceManager

--- finale/views/ResourceManager.java
+++ finale/views/ResourceManager.java
@@ -29,6 +29,7 @@ public class ResourceManager {
 	private Map<String, BufferedImage> loadedCache = new HashMap<String, BufferedImage>();
 	private Map<String, BufferedImage> scaledCache = new HashMap<String, BufferedImage>();
 	private Map<String, AudioClip> soundCache = new HashMap<String, AudioClip>();
+	private Object cacheLock = new Object();
 	
 	private ResourceManager() {
 		//
@@ -47,31 +48,65 @@ public class ResourceManager {
 	 * @param filename : the file name of the Image, no extension.
 	 */
 	public void preloadImage(final String filename) {
-		if (loadedCache.containsKey(filename)) {
-			//
-		} else {
-			loadedCache.put(filename, new BufferedImage(1,1,BufferedImage.TYPE_4BYTE_ABGR));
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					System.out.println("Loading image "+filename);
-					
-					ClassLoader cl = getClass().getClassLoader();
-					InputStream strm = cl.getResourceAsStream(filename+".jpg");
-					if (strm == null)
-						strm = cl.getResourceAsStream(filename+".png");
-					
-					try {
-						loadedCache.put(filename, ImageIO.read(strm));
-						scaledCache.remove(filename);
-					} catch (IOException e) {
-						System.err.println("Failed to load "+filename);
-						e.printStackTrace();
+		synchronized (cacheLock) {
+			if (loadedCache.containsKey(filename)) {
+				//
+			} else {
+				loadedCache.put(filename, new BufferedImage(1,1,BufferedImage.TYPE_3BYTE_BGR));
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						System.out.println("Loading image "+filename);
+						
+						ClassLoader cl = getClass().getClassLoader();
+						InputStream strm = cl.getResourceAsStream(filename+".jpg");
+						if (strm == null)
+							strm = cl.getResourceAsStream(filename+".png");
+
+//						try {
+//							Thread.sleep(1000); // simulate loading
+//						} catch (InterruptedException e) {}
+						try {
+							synchronized (cacheLock) {
+								loadedCache.put(filename, ImageIO.read(strm));
+								scaledCache.remove(filename);
+							}
+						} catch (IOException e) {
+							System.err.println("Failed to load "+filename);
+							e.printStackTrace();
+						}
 					}
-				}
-			}).start();
+				}).start();
+			}
 		}
 	}
+	public BufferedImage get(String filename, int width, int height) {
+		filename = IMG_BASE+filename;
+		preloadImage(filename);
+		return rescaleImage(filename, width, height);
+	}
+	
+	private BufferedImage rescaleImage(String filename, int width, int height) {
+		if (width < 1)
+			width = 1;
+		if (height < 1)
+			height = 1;
+
+		BufferedImage cached = scaledCache.get(filename);
+		if (cached == null || cached.getWidth() != width || cached.getHeight() != height) {
+			cached = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+			Graphics gc = cached.getGraphics();
+			synchronized (cacheLock) {
+				gc.drawImage(loadedCache.get(filename), 0, 0, width, height, null);
+			}
+			gc.dispose();
+			scaledCache.put(filename, cached);
+		}
+		return scaledCache.get(filename);
+	}
+	
+	
+	
 	public void preloadFont(String filename) {
 		if (!loadedFontCache.containsKey(filename)) {
 			System.out.println("Loading font "+filename);
@@ -83,11 +118,6 @@ public class ResourceManager {
 			}
 		}
 	}
-	public BufferedImage get(String filename, int width, int height) {
-		filename = IMG_BASE+filename;
-		preloadImage(filename);
-		return rescaleImage(filename, width, height);
-	}
 	public Font getFont(String filename) {
 		filename = FONT_BASE+filename;
 		preloadFont(filename);
@@ -97,41 +127,29 @@ public class ResourceManager {
 	public InputStream getFileStream(String filename) {
 		return getClass().getClassLoader().getResourceAsStream(filename);
 	}
-	
-	private BufferedImage rescaleImage(String filename, int width, int height) {
-		if (width < 1)
-			width = 1;
-		if (height < 1)
-			height = 1;
-		
-		BufferedImage cached = scaledCache.get(filename);
-		if (cached == null || cached.getWidth() != width || cached.getHeight() != height) {
-            cached = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
-            Graphics gc = cached.getGraphics();
-            gc.drawImage(loadedCache.get(filename), 0, 0, width, height, null);
-            gc.dispose();
-            scaledCache.put(filename, cached);
-		}
-		return scaledCache.get(filename);
-	}
 
 	public void playSound(String basename) {
 		final String filename = basename + ".wav.au";
-		if (soundCache.containsKey(filename)) {
-			soundCache.get(filename).stop();
-			soundCache.get(filename).play();
-		} else {
-			soundCache.put(filename, new NullAudioClip());
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					System.out.println("Loading sound "+filename);
-					URL url = getClass().getClassLoader().getResource(SOUND_BASE+filename);
-					AudioClip x = Applet.newAudioClip(url);
-					soundCache.put(filename, x);
-					x.play();
-				}
-			}).start();
+		synchronized (cacheLock) {
+			if (soundCache.containsKey(filename)) {
+				soundCache.get(filename).stop();
+				soundCache.get(filename).play();
+			} else {
+				soundCache.put(filename, new NullAudioClip());
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						System.out.println("Loading sound "+filename);
+						URL url = getClass().getClassLoader().getResource(SOUND_BASE+filename);
+						AudioClip x = Applet.newAudioClip(url);
+
+						synchronized (cacheLock) {
+							soundCache.put(filename, x);
+						}
+						x.play();
+					}
+				}).start();
+			}
 		}
 	}
 	class NullAudioClip implements AudioClip {
